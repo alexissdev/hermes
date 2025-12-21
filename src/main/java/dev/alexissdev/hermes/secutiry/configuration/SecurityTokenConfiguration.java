aecbb2528285050527c9b4095d50b7d53c87322d
alexissdev <costaaalexis@gmail.com>
2025-12-21 17:17:32 -0300

    docs: add the docs from token config

--- src/main/java/dev/alexissdev/hermes/secutiry/configuration/SecurityTokenConfiguration.java
+++ src/main/java/dev/alexissdev/hermes/secutiry/configuration/SecurityTokenConfiguration.java
@@ -4,6 +4,16 @@ import io.jsonwebtoken.Jwts;
 
 import javax.crypto.SecretKey;
 
+/**
+ * SecurityTokenConfiguration defines the constants and key configuration
+ * used for managing security tokens in the application.
+ *
+ * This class includes token expiration time, a secret key for signing,
+ * and standard header information required for handling authorization tokens.
+ * It is designed to centralize the token-related configurations
+ * and ensure consistency across the application.
+ */
+
 public class SecurityTokenConfiguration {
 
     public static final long EXPIRATION_TIME = 3600_000;
